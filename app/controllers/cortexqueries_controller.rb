423aad5ae8fee0b7e67b487e5b481df4688be496
Joshua Banjo <wun2393@gmail.com>
2025-10-19 15:30:00 +0100

    added backend functionality so that search query is caught and parsed to find corresponding records

--- app/controllers/cortexqueries_controller.rb
+++ app/controllers/cortexqueries_controller.rb
@@ -3,10 +3,18 @@ class CortexqueriesController < ApplicationController
   end
 
   def search
-    @staticqueries = StaticQuery.all
-    @staticquery_1 = @staticqueries[0]
-    @staticresponse_1 = @staticquery_1.static_response
-    redirect_to staticquery_staticresponse_path(@staticquery_1, @staticresponse_1)
+    req_query = params[:q].nil? ? nil : params[:q]
+    if req_query.nil?
+    else
+      @static_query = StaticQuery.where(txt_query: req_query)[0]
+      @static_response = @static_query.static_response
+      redirect_to staticquery_staticresponse_path(@static_query, @static_response)
+    end
+
+    # @staticqueries = StaticQuery.all
+    # @staticquery_1 = @staticqueries[0]
+    # @staticresponse_1 = @staticquery_1.static_response
+    # redirect_to staticquery_staticresponse_path(@staticquery_1, @staticresponse_1)
   end
 
   def show
