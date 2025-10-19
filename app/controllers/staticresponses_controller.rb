f3dc52b354de3a77ad55f3860436a2286276ef22
Joshua Banjo <wun2393@gmail.com>
2025-10-19 15:42:54 +0100

    added backend functionality to parse params and use as criteria to find records in Staticresponsescontroller

--- app/controllers/staticresponses_controller.rb
+++ app/controllers/staticresponses_controller.rb
@@ -1,4 +1,6 @@
 class StaticresponsesController < ApplicationController
   def show
+    @static_query = StaticQuery.find(params[:staticquery_id])
+    @static_response = @static_query.static_response
   end
 end
